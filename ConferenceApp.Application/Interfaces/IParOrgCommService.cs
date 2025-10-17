899bdb201d24d96511287abbfb763d4da13b9cf0
Stepanov Aleksey <Aleksey13042001@mail.ru>
2025-10-17 13:14:06 +0500

    refactor(Application): remove components for rewrite

--- ConferenceApp.Application/Interfaces/IParOrgCommService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConferenceApp.Domain.Entities;
-
-namespace ConferenceApp.Application.Interfaces
-{
-    public interface IParOrgCommService
-    {
-        List<ParOrgComm> GetAllParOrgComm();
-    }
-}
--- ConferenceApp.Application/Interfaces/IParProgramCommService.cs
+++
@@ -1,9 +0,0 @@
-﻿using ConferenceApp.Domain.Entities;
-
-namespace ConferenceApp.Application.Interfaces
-{
-    public interface IParProgramCommService
-    {
-        List<ParProgramComm> GetAllParProgramComm();
-    }
-}
