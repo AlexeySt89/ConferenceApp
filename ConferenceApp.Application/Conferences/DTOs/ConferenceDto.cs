354ae42bce134e15b7875240934d8420c1813d0c
Stepanov Aleksey <Aleksey13042001@mail.ru>
2025-10-19 18:40:29 +0500

    chore(application): move DTOs to new folders

---
+++ ConferenceApp.Application/Conferences/DTOs/ConferenceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ConferenceApp.Application.Conferences.DTOs
+{
+    public class ConferenceDto
+    {
+        public Guid Id { get; init; }
+        public string Title { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+        public DateTime Date { get; init; }
+        public int ParticipantsCount { get; init; }
+    }
+}
---
+++ ConferenceApp.Application/Participants/Queries/GetParticipants/ParticipantDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ConferenceApp.Application.Participants.Queries.GetParticipants
+{
+    public class ParticipantDto
+    {
+        public Guid Id { get; init; }
+        public string FullName { get; init; } = string.Empty;
+        public string Organization { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
+        public string TitleLecture { get; init; } = string.Empty;
+        public string Section { get; init; } = string.Empty;
+        public bool? IsApproved { get; init; }
+        public string? RejectReason { get; init; }
+    }
+}
