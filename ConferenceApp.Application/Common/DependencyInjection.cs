f4e354dd59b843a86fa7685dfc5dde6860a4dc13
Stepanov Aleksey <Aleksey13042001@mail.ru>
2025-10-23 15:00:36 +0500

    feat(application): add DI container and Logging, Validation Behavior

---
+++ ConferenceApp.Application/Common/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using ConferenceApp.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ConferenceApp.Application.Common
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
