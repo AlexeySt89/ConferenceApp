c763047ba9f2abaaa689d831a7e60e4efde63a29
Stepanov Aleksey <Aleksey13042001@mail.ru>
2025-10-23 14:38:34 +0500

    chore(infrastructure): configure dependency injection
    - Register all infrastructure services in DI container
    - Configure service lifetimes (scoped/transient/singleton)

---
+++ ConferenceApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using ConferenceApp.Domain.Interfaces.Repositories;
+using ConferenceApp.Domain.Interfaces.Services;
+using ConferenceApp.Infrastructure.Repositories;
+using ConferenceApp.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConferenceApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+            services.AddScoped<IConferenceRepository, ConferenceRepository>();
+            services.AddScoped<IParticipantRepository, ParticipantRepository>();
+            services.AddScoped<IAdminRepository, AdminRepository>();
+            services.AddScoped<IParOrgCommRepository, ParOrgCommRepository>();
+            services.AddScoped<IParProgramCommRepository, ParProgramCommRepository>();
+
+            services.AddScoped<IPasswordHasher, PasswordHasher>();
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+            return services;
+        }
+    }
+}
